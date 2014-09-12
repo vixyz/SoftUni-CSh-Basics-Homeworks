74f0ea54fa9705f5334b8bf9c1588b57370b0f91
Victoria <victoria.valkova@gmail.com>
2014-09-12 11:39:49 +0300

    Useless comment
    This can be deleted. Doesn't give any information about the program

--- 01-Intro-Programming-Homework/HelloCSharp/Program.cs
+++ 01-Intro-Programming-Homework/HelloCSharp/Program.cs
@@ -7,7 +7,7 @@
             Console.Write("Hello C#!\n"); //Writes text on a row
             // I was here to play with my new GitHub :)
             // It looks great! Let's try again
-            // I don't understand any more why people scare of GitHub
+            // Next step: console practice
         }
     }
 
