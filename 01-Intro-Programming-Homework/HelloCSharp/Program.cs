a0df7fa9e5e89c83993f23e863f92355dbe1f546
Victoria <victoria.valkova@gmail.com>
2014-09-12 11:38:14 +0300

    Added comment
    An useless one

--- 01-Intro-Programming-Homework/HelloCSharp/Program.cs
+++ 01-Intro-Programming-Homework/HelloCSharp/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.Write("Hello C#!\n"); //Writes text on a row
             // I was here to play with my new GitHub :)
+            // It looks great! Let's try again
+            // I don't understand any more why people scare of GitHub
         }
     }
 
