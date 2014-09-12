085652e55128089b3e7d4cd558f8286afc7a0e64
Victoria <victoria.valkova@gmail.com>
2014-09-12 03:29:12 +0300

    Added comment
    Added a new comment to play with GitHub

--- 01-Intro-Programming-Homework/HelloCSharp/Program.cs
+++ 01-Intro-Programming-Homework/HelloCSharp/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.Write("Hello C#!\n"); //Writes text on a row
+            // I was here to play with my new GitHub :)
         }
     }
 
